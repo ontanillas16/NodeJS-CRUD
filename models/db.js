837bfd0d85ad0b0f721d01d5ecf91a0bb75426a5
ontanillas16 <79128851+ontanillas16@users.noreply.github.com>
2021-07-31 18:50:28 +0800

    Add files via upload

---
+++ models/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+
+
+mongoose.connect('mongodb://localhost:27017/EmployeeDB', {useNewUrlParser: true, useUnifiedTopology: true},(err) =>{
+    if(!err){console.log('MongoDB Connection Succeeded.')}
+    else{console.log('Error in MongoDB Connection: '+ err)}
+});
+
+require('./employee.model');
